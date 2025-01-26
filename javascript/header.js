789fdb3b116b5d00a374021f8e0f4787eb05469d
Marsuazo23 <marcesuazo2004@gmail.com>
2025-01-26 13:39:57 -0600

    Carpeta de Javascript

--- javascript/header.js
+++ javascript/header.js
@@ -1,20 +1,25 @@
 function loadHeader() {
-    /* Definimos el contenido HTML del encabezado en una variable (headerHTML) */
+    /*Definimos el contenido HTML del encabezado en una variable (headerHTML)*/
     const headerHTML = `
         <header>
             <nav>
-                <ul class="menu">
-                    <li class="logo">
-                        <img src="imagenes/logo.png" alt="Logo" />
-                    </li>
+                <div class="left-container">
+                    <div class="Perfil">
+                        <img src="imagenes/imagen_perfil.jpeg" alt="Imagen Perfil" />
+                    </div>
+                    <h1><em><strong>
+                        Marcela Suazo
+                    </h1></em></strong>
+                </div>
+                <ul class="menu-izquierda">
                     <li><a href="index.html">Inicio</a></li>
-                    <li><a href="sobremi.html">Sobre mí</a></li>
-                    <li><a href="proyectos.html">Proyectos</a></li>
-                    <li><a href="contactame.html">Contáctame</a></li>
+                    <li><a href="SobreMi.html">Sobre mí</a></li>
+                    <li><a href="Proyectos.html">Proyectos</a></li>
+                    <li><a href="Contactame.html">Contáctame</a></li>
                 </ul>
             </nav>
         </header>
     `;
-    /* Insertamos el contenido HTML generado en el contenedor con id 'header-container' */
+    /*Insertamos el contenido HTML generado en el contenedor con id 'header-container'*/
     document.getElementById('header-container').innerHTML = headerHTML;
 }
